e59c2f5c951561e05c4f44fbfd1f2c89018c112e
jrosmit <jrosmit@gmail.com>
2024-07-03 22:55:24 -0400

    netlify attempt 03

--- ex01 - Social Link Profile/netlify/functions/index.ts
+++ ex01 - Social Link Profile/netlify/functions/index.ts
@@ -2,7 +2,6 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 
 // initialize express
-const express = require('express')
 const app = express()
 const port = 3000
 
@@ -10,5 +9,8 @@ const port = 3000
 app.use(express.static('../public'))
 app.use(express.static('../assets/images'))
 app.use(express.static('../assets/css'))
+
+const router = Router();
+router.get("/", (req, res) => res.send("Hello World!"));
 
 export const handler = serverless(app);
