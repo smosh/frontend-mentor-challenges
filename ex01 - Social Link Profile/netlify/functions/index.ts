25857e0278f47af4c31e4b31723d82d5cb455137
jrosmit <jrosmit@gmail.com>
2024-07-03 22:39:24 -0400

    netlify attempt 02

--- ex01 - Social Link Profile/netlify/functions/index.ts
+++ ex01 - Social Link Profile/netlify/functions/index.ts
@@ -7,8 +7,8 @@ const app = express()
 const port = 3000
 
 // Setup Express
-app.use(express.static('public'))
-app.use(express.static('assets/images'))
-app.use(express.static('assets/css'))
+app.use(express.static('../public'))
+app.use(express.static('../assets/images'))
+app.use(express.static('../assets/css'))
 
 export const handler = serverless(app);
